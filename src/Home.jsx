760685fc3b092e242603752d2980c5727d77f31e
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-09-13 14:14:32 +0700

    draft

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
 import Layout from "./Layout";
 import "./style.css";
-import Header from "./Header-user";
+// import Header from "./Header-user";
+import HeaderNormal from "./Header-normal";
+import { useState } from "react";
 
 const mockEmployees = [
   {
@@ -24,9 +26,24 @@ const mockEmployees = [
 ];
 
 const Home = () => {
+  const [sector, setSector] = useState("");
+  const [employee, setEmployee] = useState();
+  const handleClick = (value) => {
+    setSector(value);
+  };
+
   return (
     <Layout>
-      <Header />
+      <div className="header">
+        <h1>Generation Thailand</h1>
+        <h1>React - Assessment</h1>
+      </div>
+      <div>
+        <button onClick={() => handleClick("user")} className="home-button">
+          User Home Sector
+        </button>
+        <button onClick={() => handleClick("admin")} className="home-button">Admin Home Sector</button>
+      </div>
     </Layout>
   );
 };
